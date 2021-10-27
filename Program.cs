975cec5b0fe643b16931a0fedc2eca46cfb7ae9c
Elessar7 <92802554+Elessar7@users.noreply.github.com>
2021-10-27 22:22:51 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Домашняя_работа
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            String name = Console.ReadLine();
+            Console.WriteLine($"Привет,{name}, сегодня " + DateTime.Now); // Основное задание
+
+            string[] m = new string[100]; // Дополнительное задание
+            for (int i = 1; i < m.Length; i++)
+            {
+                m[i] = i.ToString();
+                if (i % 3 == 0)
+                {
+                    m[i] = "fizz";
+                }
+                if (i % 5 == 0)
+                {
+                    m[i] = "buzz";
+                }
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    m[i] = "fizzbuzz";
+                }
+                Console.WriteLine(m[i]);
+            }
+        }
+    }
+}
