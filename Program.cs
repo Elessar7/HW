0f7888e2eab52c5ce5ba0913a93f0faa39f9596b
Elessar7 <92802554+Elessar7@users.noreply.github.com>
2021-10-31 21:45:11 +0500

    2_2

--- Program.cs
+++ Program.cs
@@ -1,33 +1,63 @@
 ﻿using System;
 
-namespace Домашняя_работа
+namespace Проект_2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("What is your name?");
-            String name = Console.ReadLine();
-            Console.WriteLine($"Привет,{name}, сегодня " + DateTime.Now); // Основное задание
-
-            string[] m = new string[100]; // Дополнительное задание
-            for (int i = 1; i < m.Length; i++)
+            int n;
+            n = int.Parse(Console.ReadLine());
+            if (n < 1)
             {
-                m[i] = i.ToString();
-                if (i % 3 == 0)
-                {
-                    m[i] = "fizz";
-                }
-                if (i % 5 == 0)
-                {
-                    m[i] = "buzz";
-                }
-                if (i % 3 == 0 && i % 5 == 0)
-                {
-                    m[i] = "fizzbuzz";
-                }
-                Console.WriteLine(m[i]);
+                Console.WriteLine("Введите положительно число от 1 до 12");
             }
+            else
+            {
+                switch (n )
+                {
+                    case 1:
+                        Console.WriteLine("Январь");
+                        break;
+                    case 2:
+                        Console.WriteLine("Февраль");
+                        break;
+                    case 3:
+                        Console.WriteLine("Март");
+                        break;
+                    case 4:
+                        Console.WriteLine("Апрель");
+                        break;
+                    case 5:
+                        Console.WriteLine("Май");
+                        break;
+                    case 6:
+                        Console.WriteLine("Июнь");
+                        break;
+                    case 7:
+                        Console.WriteLine("Июль");
+                        break;
+                    case 8:
+                        Console.WriteLine("Август");
+                        break;
+                    case 9:
+                        Console.WriteLine("Сентябрь");
+                        break;
+                    case 10:
+                        Console.WriteLine("Октябрь");
+                        break;
+                    case 11:
+                        Console.WriteLine("Ноябрь");
+                        break;
+                    case 12:
+                        Console.WriteLine("Декабрь");
+                        break;
+                }
+            }
+
+
+
         }
+        
     }
 }
